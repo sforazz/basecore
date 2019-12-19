426ec1fbaa42d016ab3540d4ef8f4250ca0f2b29
Francesco Sforazzini <francesco.sforazzini@gmail.com>
2019-12-19 16:30:12 +0100

    debugging

--- basecore/database/pyxnat.py
+++ basecore/database/pyxnat.py
@@ -9,6 +9,7 @@ def put(project, subject, session, scan, config=None, url=None, pwd=None, user=N
 
     session_modality_re = re.compile(r'(MR|CT|RT)(\d+|\w+)')
     scan_name = scan.split('/')[-1].split('.nii.gz')[0]
+    print(scan_name)
     match = session_modality_re.match(session)
     res_format = get_resource_name(scan)
     proc = ''
