9397f12a9281801e4cfd977533c9af7dd1253d40
Francesco Sforazzini <francesco.sforazzini@gmail.com>
2020-01-24 16:12:32 +0100

    added fslreorient2std in bet workflow

--- basecore/workflows/bet.py
+++ basecore/workflows/bet.py
@@ -2,6 +2,7 @@
 import nipype
 from basecore.interfaces.mic import HDBet
 from basecore.workflows.datahandler import datasink_base
+from nipype.interfaces.fsl.utils import Reorient2Std
 
 
 def brain_extraction(sub_id, datasource, sessions,
@@ -10,11 +11,16 @@ def brain_extraction(sub_id, datasource, sessions,
     bet = nipype.MapNode(interface=HDBet(), iterfield=['input_file'], name='bet')
     bet.inputs.save_mask = 1
     bet.inputs.out_file = 'T1_preproc'
+    
+    reorient = nipype.MapNode(interface=Reorient2Std(), iterfield=['in_file'],
+                              name='reorient')
 
     if t10:
         bet_t10 = nipype.Node(interface=HDBet(), name='t1_0_bet')
         bet_t10.inputs.save_mask = 1
         bet_t10.inputs.out_file = 'T1_0_bet'
+        reorient_t10 = nipype.MapNode(interface=Reorient2Std(), iterfield=['in_file'],
+                              name='reorient_t10')
 
     datasink = nipype.Node(nipype.DataSink(base_directory=RESULT_DIR), "datasink")
 
@@ -27,9 +33,11 @@ def brain_extraction(sub_id, datasource, sessions,
     # Create Workflow
     workflow = nipype.Workflow('brain_extraction_workflow', base_dir=NIPYPE_CACHE)
 
-    workflow.connect(datasource, 't1', bet, 'input_file')
+    workflow.connect(datasource, 't1', reorient, 'in_file')
+    workflow.connect(reorient, 'out_file', bet, 'input_file')
     if t10:
-        workflow.connect(datasource, 't1_0', bet_t10, 'input_file')
+        workflow.connect(datasource, 't1_0', reorient_t10, 'in_file')
+        workflow.connect(reorient_t10, 'out_file', bet_t10, 'input_file')
         workflow.connect(bet_t10, 'out_file', datasink,
                          'results.subid.T10.@T1_ref_bet')
 
---
+++ scripts/anal_data_preparation_workflow.py
@@ -0,0 +1,69 @@
+from basecore.interfaces.utils import DicomCheck, ConversionCheck
+import os
+import nipype
+import nipype.interfaces.utility as util
+from nipype.interfaces.dcm2nii import Dcm2niix
+
+
+rt_files = ['RTSTRUCT', 'RTDOSE', 'RTPLAN']
+ct_files = ['RTCT']
+base_dir = '/mnt/sdb/anal-ca_SORTED_RT'
+result_dir = '/mnt/sdb/anal_converted'
+cache_dir = '/mnt/sdb/anal_converted_cache'
+
+if not os.path.isdir(result_dir):
+    os.mkdir(result_dir)
+
+datasource_ct = nipype.Node(
+    interface=nipype.DataGrabber(infields=['name'], outfields=['directory']),
+    name='datasource_ct')
+datasource_ct.inputs.base_directory = base_dir
+datasource_ct.inputs.template = '*'
+datasource_ct.inputs.sort_filelist = True
+datasource_ct.inputs.field_template = dict(directory='*/*/%s/1-*')
+datasource_ct.inputs.name = 'RTCT'
+
+inputnode_rt = nipype.Node(
+    interface=util.IdentityInterface(fields=['rt_files']),
+    name='inputnode_rt')
+inputnode_rt.iterables = ('rt_files', rt_files)
+
+datasource_rt = nipype.Node(
+    interface=nipype.DataGrabber(infields=['rt_files'], outfields=['directory']),
+    name='datasource_rt')
+datasource_rt.inputs.base_directory = base_dir
+datasource_rt.inputs.template = '*'
+datasource_rt.inputs.sort_filelist = True
+datasource_rt.inputs.field_template = dict(directory='*/*/%s')
+
+dc_rt = nipype.MapNode(interface=DicomCheck(), iterfield=['dicom_dir'], name='dc_rt')
+dc_rt.inputs.working_dir = result_dir
+
+dc_ct = nipype.MapNode(interface=DicomCheck(), iterfield=['dicom_dir'], name='dc_ct')
+dc_ct.inputs.working_dir = result_dir
+
+converter_ct = nipype.MapNode(interface=Dcm2niix(),
+                           iterfield=['source_dir', 'out_filename', 'output_dir'],
+                           name='converter_ct')
+converter_ct.inputs.compress = 'y'
+converter_ct.inputs.philips_float = False
+converter_ct.inputs.merge_imgs = True
+
+check_ct = nipype.MapNode(interface=ConversionCheck(),
+                          iterfield=['in_file', 'file_name'],
+                          name='check_conversion_ct')
+
+workflow = nipype.Workflow('data_preparation_workflow', base_dir=cache_dir)
+workflow.connect(inputnode_rt, 'rt_files', datasource_rt, 'rt_files')
+workflow.connect(datasource_rt, 'directory', dc_rt, 'dicom_dir')
+workflow.connect(datasource_ct, 'directory', dc_ct, 'dicom_dir')
+workflow.connect(dc_ct, 'outdir', converter_ct, 'source_dir')
+workflow.connect(dc_ct, 'scan_name', converter_ct, 'out_filename')
+workflow.connect(dc_ct, 'base_dir', converter_ct, 'output_dir')
+workflow.connect(dc_ct, 'scan_name', check_ct, 'file_name')
+workflow.connect(converter_ct, 'converted_files', check_ct, 'in_file')
+
+# workflow.run()
+workflow.run('MultiProc', plugin_args={'n_procs': 8})
+
+print('Done!')
